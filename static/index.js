2ccb7e6b07e5c683a393dc86da062f17668ebae1
KoffeeJava <85138593+KoffeeJava@users.noreply.github.com>
2025-03-15 13:49:34 -0600

    wrong index.js file :(

--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-alert("Warning!\n The link ez.entity.lol is now outdated!\n Use ez.koffeejava.lol!")
+
 
 const form = document.querySelector('form');
 const input = document.querySelector('input');
